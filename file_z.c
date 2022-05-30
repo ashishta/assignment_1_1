12bed3ee087d8526948b94fabd639bbbfc6ad60e
Ashish Yadav <ashish.yadav@tigeranalytics.com>
2022-05-30 12:47:57 +0530

    Bug fix

--- file_z.c
+++ file_z.c
@@ -1,5 +1,6 @@
 # include stdio
 
 int main {
-    printf("Hello world! I'm fixing an critical issue here.")
+    printf("Hello world! I'm fixing an critical issue here.");
+    printf("Fixed the bug in above line for another commit");
 }
